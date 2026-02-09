551213adbcd90d2948527cbe4badaaebc108d053
Mariano <marianopainefil@gmail.com>
2026-02-09 09:53:48 -0300

    Hero component added

--- riu-challenge/src/app/pages/home/home-page.component.ts
+++ riu-challenge/src/app/pages/home/home-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeroComponent } from '../../components/hero/hero.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [HeroComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
