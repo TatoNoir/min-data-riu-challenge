8e16036c19e31efee43c10beb74b21fbeee70817
Mariano <marianopainefil@gmail.com>
2026-02-09 12:49:21 -0300

    products component created

--- riu-challenge/src/app/pages/home/home-page.component.ts
+++ riu-challenge/src/app/pages/home/home-page.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeroComponent } from '../../components/hero/hero.component';
 import { CardsGridComponent } from '../../components/cards-grid/cards-grid.component';
 import { CardItemComponent } from '../../components/card-item/card-item.component';
+import { ProductsPreviewComponent } from '../../components/products-preview/products-preview.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [HeroComponent, CardsGridComponent, CardItemComponent],
+  imports: [HeroComponent, CardsGridComponent, ProductsPreviewComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
